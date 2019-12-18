0bdabf4c2f1e6a5953ea8a755650de5946b2b205
AymericDuvivier <58727522+AymericDuvivier@users.noreply.github.com>
2019-12-18 14:03:55 +0100

    Update build-rpm.sh

--- scripts/build-rpm.sh
+++ scripts/build-rpm.sh
@@ -4,7 +4,8 @@
 set -e
 source config
 
-NAME="python2-comodit-client"
+NAME="comodit-client"
+ENV="python2"
 
 git branch
 
@@ -25,9 +26,9 @@ fi
 
 COMMIT=`git describe --tags --long --match "release-$VERSION" | awk -F"-" '{print $4}'`
 
-sed "s/#VERSION#/${VERSION}/g" rpmbuild/SPECS/${NAME}.spec.template > rpmbuild/SPECS/${NAME}.spec
-sed -i "s/#RELEASE#/${RELEASE}/g" rpmbuild/SPECS/${NAME}.spec
-sed -i "s/#COMMIT#/${COMMIT}/g" rpmbuild/SPECS/${NAME}.spec
+sed "s/#VERSION#/${VERSION}/g" rpmbuild/SPECS/${NAME}.spec.template > rpmbuild/SPECS/${ENV}-${NAME}.spec
+sed -i "s/#RELEASE#/${RELEASE}/g" rpmbuild/SPECS/${ENV}-${NAME}.spec
+sed -i "s/#COMMIT#/${COMMIT}/g" rpmbuild/SPECS/${ENV}-${NAME}.spec
 
 # Generate version file
 echo "VERSION=\""$VERSION"\"" > comodit_client/version.py
@@ -36,7 +37,7 @@ echo "RELEASE=\""$RELEASE"\"" >> comodit_client/version.py
 mkdir -p rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 
 # Do not tar directly in SOURCES directory to escape error
-tar -cvzf ${NAME}-${VERSION}-${RELEASE}.tar.gz * \
+tar -cvzf ${ENV}-${NAME}-${VERSION}-${RELEASE}.tar.gz * \
 --exclude .git \
 --exclude debian \
 --exclude scripts \
@@ -45,7 +46,7 @@ tar -cvzf ${NAME}-${VERSION}-${RELEASE}.tar.gz * \
 --exclude gitignorel \
 --exclude gitmodules 
 
-mv ${NAME}-${VERSION}-${RELEASE}.tar.gz rpmbuild/SOURCES
+mv ${ENV}-${NAME}-${VERSION}-${RELEASE}.tar.gz rpmbuild/SOURCES
 
 rpmbuild --define "_topdir $(pwd)/rpmbuild" -ba rpmbuild/SPECS/${NAME}.spec
 
@@ -53,14 +54,14 @@ if [ -f "/usr/bin/mock" ]
 then
   for platform in "${PLATFORMS[@]}"
   do
-    /usr/bin/mock -r ${platform} --rebuild rpmbuild/SRPMS/${NAME}-${VERSION}-${RELEASE}*.src.rpm
+    /usr/bin/mock -r ${platform} --rebuild rpmbuild/SRPMS/${ENV}-${NAME}-${VERSION}-${RELEASE}*.src.rpm
     mkdir -p ${HOME}/packages/${platform}
     mv /var/lib/mock/${platform}/result/*.rpm ${HOME}/packages/${platform}
   done
 
   for platform in "${SYSTEMD_PLATFORMS[@]}"
   do
-    /usr/bin/mock --bootstrap-chroot -r ${platform} --define "use_systemd 1" --rebuild rpmbuild/SRPMS/${NAME}-${VERSION}-${RELEASE}*.src.rpm
+    /usr/bin/mock --bootstrap-chroot -r ${platform} --define "use_systemd 1" --rebuild rpmbuild/SRPMS/${ENV}-${NAME}-${VERSION}-${RELEASE}*.src.rpm
     mkdir -p ${HOME}/packages/${platform}
     mv /var/lib/mock/${platform}/result/*.rpm ${HOME}/packages/${platform}
   done
