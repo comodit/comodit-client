13b8ce3d94315204149b88b6e11dd5d3d3c59ed7
Aymeric Duvivier De Fortemps <aduvivier@localhost.localdomain>
2019-12-18 14:14:12 +0100

    Update python2 naming outpout

--- scripts/build-rpm.sh
+++ scripts/build-rpm.sh
@@ -26,7 +26,8 @@ fi
 
 COMMIT=`git describe --tags --long --match "release-$VERSION" | awk -F"-" '{print $4}'`
 
-sed "s/#VERSION#/${VERSION}/g" rpmbuild/SPECS/${NAME}.spec.template > rpmbuild/SPECS/${ENV}-${NAME}.spec
+sed "s/#NAME#/${ENV}${NAME}/g" rpmbuild/SPECS/${NAME}.spec.template > rpmbuild/SPECS/${ENV}-${NAME}.spec
+sed -i "s/#VERSION#/${VERSION}/g" rpmbuild/SPECS/${ENV}-${NAME}.spec
 sed -i "s/#RELEASE#/${RELEASE}/g" rpmbuild/SPECS/${ENV}-${NAME}.spec
 sed -i "s/#COMMIT#/${COMMIT}/g" rpmbuild/SPECS/${ENV}-${NAME}.spec
 
