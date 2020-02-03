0fe8ce6b30350fe3780f8a4926ce587be9150238
Aymeric Duvivier <aymeric.duvivier@guardis.com>
2020-02-03 15:58:52 +0000

    Update compilation to manage both python2 and python3

--- scripts/build-rpm.sh
+++ scripts/build-rpm.sh
@@ -5,7 +5,6 @@ set -e
 source config
 
 NAME="comodit-client"
-ENV="python"
 
 git branch
 
@@ -26,10 +25,9 @@ fi
 
 COMMIT=`git describe --tags --long --match "release-$VERSION" | awk -F"-" '{print $4}'`
 
-sed "s/#NAME#/${NAME}/g" rpmbuild/SPECS/${ENV}-${NAME}.spec.template > rpmbuild/SPECS/${ENV}-${NAME}.spec
-sed -i "s/#VERSION#/${VERSION}/g" rpmbuild/SPECS/${ENV}-${NAME}.spec
-sed -i "s/#RELEASE#/${RELEASE}/g" rpmbuild/SPECS/${ENV}-${NAME}.spec
-sed -i "s/#COMMIT#/${COMMIT}/g" rpmbuild/SPECS/${ENV}-${NAME}.spec
+sed "s/#VERSION#/${VERSION}/g" rpmbuild/SPECS/${NAME}.spec.template > rpmbuild/SPECS/${NAME}.spec
+sed -i "s/#RELEASE#/${RELEASE}/g" rpmbuild/SPECS/${NAME}.spec
+sed -i "s/#COMMIT#/${COMMIT}/g" rpmbuild/SPECS/${NAME}.spec
 
 # Generate version file
 echo "VERSION=\""$VERSION"\"" > comodit_client/version.py
@@ -38,7 +36,7 @@ echo "RELEASE=\""$RELEASE"\"" >> comodit_client/version.py
 mkdir -p rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 
 # Do not tar directly in SOURCES directory to escape error
-tar -cvzf ${ENV}-${NAME}-${VERSION}-${RELEASE}.tar.gz * \
+tar -cvzf ${NAME}-${VERSION}-${RELEASE}.tar.gz * \
 --exclude .git \
 --exclude debian \
 --exclude scripts \
@@ -47,22 +45,22 @@ tar -cvzf ${ENV}-${NAME}-${VERSION}-${RELEASE}.tar.gz * \
 --exclude gitignorel \
 --exclude gitmodules 
 
-mv ${ENV}-${NAME}-${VERSION}-${RELEASE}.tar.gz rpmbuild/SOURCES
+mv ${NAME}-${VERSION}-${RELEASE}.tar.gz rpmbuild/SOURCES
 
-rpmbuild --define "_topdir $(pwd)/rpmbuild" -ba rpmbuild/SPECS/${ENV}-${NAME}.spec
+rpmbuild --define "_topdir $(pwd)/rpmbuild" -ba rpmbuild/SPECS/${NAME}.spec
 
 if [ -f "/usr/bin/mock" ]
 then
   for platform in "${PLATFORMS[@]}"
   do
-    /usr/bin/mock -r ${platform} --rebuild rpmbuild/SRPMS/${ENV}-${NAME}-${VERSION}-${RELEASE}*.src.rpm
+    /usr/bin/mock -r ${platform} --rebuild rpmbuild/SRPMS/${NAME}-${VERSION}-${RELEASE}*.src.rpm
     mkdir -p ${HOME}/packages/${platform}
     mv /var/lib/mock/${platform}/result/*.rpm ${HOME}/packages/${platform}
   done
 
   for platform in "${SYSTEMD_PLATFORMS[@]}"
   do
-    /usr/bin/mock --bootstrap-chroot -r ${platform} --define "use_systemd 1" --rebuild rpmbuild/SRPMS/${ENV}-${NAME}-${VERSION}-${RELEASE}*.src.rpm
+    /usr/bin/mock --bootstrap-chroot -r ${platform} --define "use_systemd 1" --rebuild rpmbuild/SRPMS/${NAME}-${VERSION}-${RELEASE}*.src.rpm
     mkdir -p ${HOME}/packages/${platform}
     mv /var/lib/mock/${platform}/result/*.rpm ${HOME}/packages/${platform}
   done
