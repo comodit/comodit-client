32dcc544eb282e33ed4f100530716f0270b8fdb4
Daniel Bartz <daniel@bartz.be>
2020-02-04 14:47:27 +0100

    Fix directory for DEV packages

--- scripts/build-dev-rpm-withPython3.sh
+++ scripts/build-dev-rpm-withPython3.sh
@@ -4,32 +4,29 @@
 set -e
 source config
 
-NAME="comodit-client"
-ENV="python"
-
-git branch
+NAME="python-comodit-client"
 
 if [ -z $1 ]
 then
-  # Get the latest tag on the current branch
-  VERSION=`git describe --abbrev=0 --tags --match "*[^dev]" | awk -F"-" '{print $2}'`
+  # Get the latest release*dev tag  
+  VERSION=`git describe --long --match "release*dev" | awk -F"-" '{print $2}'`
 else
   VERSION=$1
 fi
 
 if [ -z $2 ]
 then
-  RELEASE=1
+  # How much commit since last release*dev tag ?
+  RELEASE=`git describe --long --match "release*dev" | awk -F"-" '{print $3}'`
 else
   RELEASE=$2
 fi
 
-COMMIT=`git describe --tags --long --match "release-$VERSION" | awk -F"-" '{print $4}'`
+COMMIT=`git describe --long --match "release*dev" | awk -F"-" '{print $4}'`
 
-sed "s/#NAME#/${NAME}/g" rpmbuild/SPECS/${ENV}-${NAME}.spec.template > rpmbuild/SPECS/${ENV}-${NAME}.spec
-sed -i "s/#VERSION#/${VERSION}/g" rpmbuild/SPECS/${ENV}-${NAME}.spec
-sed -i "s/#RELEASE#/${RELEASE}/g" rpmbuild/SPECS/${ENV}-${NAME}.spec
-sed -i "s/#COMMIT#/${COMMIT}/g" rpmbuild/SPECS/${ENV}-${NAME}.spec
+sed "s/#VERSION#/${VERSION}/g" rpmbuild/SPECS/${NAME}.spec.template > rpmbuild/SPECS/${NAME}.spec
+sed -i "s/#RELEASE#/${RELEASE}/g" rpmbuild/SPECS/${NAME}.spec
+sed -i "s/#COMMIT#/${COMMIT}/g" rpmbuild/SPECS/${NAME}.spec
 
 # Generate version file
 echo "VERSION=\""$VERSION"\"" > comodit_client/version.py
@@ -38,7 +35,7 @@ echo "RELEASE=\""$RELEASE"\"" >> comodit_client/version.py
 mkdir -p rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 
 # Do not tar directly in SOURCES directory to escape error
-tar -cvzf ${ENV}-${NAME}-${VERSION}-${RELEASE}.tar.gz * \
+tar -cvzf ${NAME}-${VERSION}-${RELEASE}.tar.gz * \
 --exclude .git \
 --exclude debian \
 --exclude scripts \
@@ -47,23 +44,16 @@ tar -cvzf ${ENV}-${NAME}-${VERSION}-${RELEASE}.tar.gz * \
 --exclude gitignorel \
 --exclude gitmodules 
 
-mv ${ENV}-${NAME}-${VERSION}-${RELEASE}.tar.gz rpmbuild/SOURCES
+mv ${NAME}-${VERSION}-${RELEASE}.tar.gz rpmbuild/SOURCES
 
-rpmbuild --define "_topdir $(pwd)/rpmbuild" -ba rpmbuild/SPECS/${ENV}-${NAME}.spec
+rpmbuild --define "_topdir $(pwd)/rpmbuild" -ba rpmbuild/SPECS/${NAME}.spec
 
 if [ -f "/usr/bin/mock" ]
 then
   for platform in "${PLATFORMS[@]}"
   do
-    /usr/bin/mock -r ${platform} --rebuild rpmbuild/SRPMS/${ENV}-${NAME}-${VERSION}-${RELEASE}*.src.rpm
-    mkdir -p ${HOME}/packages/${platform}
-    mv /var/lib/mock/${platform}/result/*.rpm ${HOME}/packages/${platform}
-  done
-
-  for platform in "${SYSTEMD_PLATFORMS[@]}"
-  do
-    /usr/bin/mock --bootstrap-chroot -r ${platform} --define "use_systemd 1" --rebuild rpmbuild/SRPMS/${ENV}-${NAME}-${VERSION}-${RELEASE}*.src.rpm
-    mkdir -p ${HOME}/packages/${platform}
-    mv /var/lib/mock/${platform}/result/*.rpm ${HOME}/packages/${platform}
+    /usr/bin/mock --bootstrap-chroot -r ${platform} --rebuild rpmbuild/SRPMS/${NAME}-${VERSION}-${RELEASE}*.src.rpm
+    mkdir -p ${HOME}/packages-dev/${platform}
+    mv /var/lib/mock/${platform}/result/*.rpm ${HOME}/packages-dev/${platform}
   done
 fi
