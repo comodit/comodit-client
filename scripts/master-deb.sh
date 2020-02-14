5e036c7845347303ecaf97a616f5e069813fc5a7
AymericDuvivier <aymeric.duvivier@guardis.com>
2020-02-14 13:07:46 +0000

    Update scripts

--- scripts/master-deb.sh
+++ scripts/master-deb.sh
@@ -2,18 +2,7 @@
 
 set -e
 
-if [[ $1 == dev ]]
-then	
-  scripts/build-dev-deb.sh
-  scripts/build-dev-deb-python2.sh 
-  scripts/build-dev-deb-python3.sh
-fi
-if [[ $1 == prod ]]
-then
-  scripts/build-deb.sh	
-  scripts/build-deb-python2.sh 
-  scripts/build-deb-python3.sh
-fi
+[[ $1 == dev ]] && scripts/build-dev-deb.sh 
+[[ $1 == prod ]] && scripts/build-deb.sh 
 
 scripts/build-all-deb.sh $1
-scripts/build-all-deb-withPython3 $1
