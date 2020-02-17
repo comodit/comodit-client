9b9109d7f05e19ebc87cd14a86d80e94ecfa5136
AymericDuvivier <aymeric.duvivier@guardis.com>
2020-02-17 12:56:21 +0000

    final python3 compatibility fix

--- scripts/master-deb-withPython3.sh
+++ scripts/master-deb-withPython3.sh
@@ -3,6 +3,6 @@
 set -e
 
 [[ $1 == dev ]] && scripts/build-dev-deb-python2.sh && scripts/build-dev-deb-python3.sh
-[[ $1 == prod ]] && scripts/build-deb.sh 
+[[ $1 == prod ]] && scripts/build-deb-python2.sh && scripts/build-deb-python3.sh
 
 scripts/build-all-deb-withPython3.sh $1
