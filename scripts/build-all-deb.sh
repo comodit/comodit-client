fc65e4e95ce3f0cf45cbca16b1544cf1c974711c
maxime bourdouxhe <mbourdouxhe@willow.angleur.guardis.be>
2020-05-06 17:05:11 +0200

    Add buster for cowbuiler

--- scripts/build-all-deb.sh
+++ scripts/build-all-deb.sh
@@ -8,11 +8,11 @@ PACKAGES=packages-dev
 for tab in "${TABS[@]}"
 do
   # Create distribution directory if not exist
-  sudo mkdir -p /var/cache/pbuilder/$tab-amd64
-  sudo mkdir -p /var/cache/pbuilder/$tab-i386
+  sudo mkdir -p /var/cache/pbuilder/$tab-amd64/aptcache
+  sudo mkdir -p /var/cache/pbuilder/$tab-i386/aptcache
 
   # If it is a Debian distribution else it is Ubuntu
-  if [ $tab = 'wheezy' ] || [ $tab = 'jessie' ]; then
+  if [ $tab = 'wheezy' ] || [ $tab = 'jessie' ] || [ $tab = 'stretch' ] || [ $tab = 'buster' ]; then
     # Create base.cow distribution 
     sudo HOME=/home/$USERNAME DIST=$tab /usr/sbin/cowbuilder --create --basepath /var/cache/pbuilder/$tab-amd64/base.cow --distribution $tab --debootstrapopts --arch --debootstrapopts amd64
     sudo HOME=/home/$USERNAME DIST=$tab /usr/sbin/cowbuilder --create --basepath /var/cache/pbuilder/$tab-i386/base.cow --distribution $tab --debootstrapopts --arch --debootstrapopts i386
