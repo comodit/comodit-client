1be72f5826b2c046d313be683128e075b9bc2834
AymericDuvivier <58727522+AymericDuvivier@users.noreply.github.com>
2021-12-06 15:03:32 +0100

    Update build-all-deb.sh

--- scripts/build-all-deb.sh
+++ scripts/build-all-deb.sh
@@ -12,7 +12,7 @@ do
   sudo mkdir -p /var/cache/pbuilder/$tab-i386/aptcache
 
   # If it is a Debian distribution else it is Ubuntu
-  if [ $tab = 'wheezy' ] || [ $tab = 'jessie' ] || [ $tab = 'stretch' ] || [ $tab = 'buster' ]; then
+  if [ $tab = 'stretch' ] || [ $tab = 'buster' ] || [ $tab = 'bullseye' ]; then
     # Create base.cow distribution 
     sudo HOME=/home/$USERNAME DIST=$tab /usr/sbin/cowbuilder --create --basepath /var/cache/pbuilder/$tab-amd64/base.cow --distribution $tab --debootstrapopts --arch --debootstrapopts amd64
     sudo HOME=/home/$USERNAME DIST=$tab /usr/sbin/cowbuilder --create --basepath /var/cache/pbuilder/$tab-i386/base.cow --distribution $tab --debootstrapopts --arch --debootstrapopts i386
